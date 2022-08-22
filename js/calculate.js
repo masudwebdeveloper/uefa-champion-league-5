b892fb8c953e836931eed089c8838a4db94dae43
masudrana5 <abdullainfo30@gmail.com>
2022-08-22 10:09:45 +0600

    calculate total expenses added done

--- js/calculate.js
+++ js/calculate.js
@@ -8,9 +8,11 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
    // const playerInputFieldId = document.getElementById('player-input-field');
    // const playerInputValueString = playerInputFieldId.value;
    // const playerInputValue = parseFloat(playerInputValueString);
+   const playerInputValue = getBudgetInputField('player-input-field')
+   console.log(playerInputValue);
    const listItems = document.getElementById('list-items');
    const child = listItems.children.length;
-   if (!playerInputValueString) {
+   if (!playerInputValue) {
       alert('provide the input fill');
       return;
    } else if (isNaN(playerInputValue)) {
@@ -24,5 +26,30 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
    const playerCost = playerInputValue * child;
    const playerTotalCost = playerCost + previousExpenses;
    elementValueId.innerText = playerTotalCost;
+})
+
+document.getElementById('total-btn').addEventListener('click', function () {
+   const managerInputValue = getBudgetInputField('manager-input-field');
+   const coachInputValue = getBudgetInputField('coach-input-field');
+   const managerAndCoachExpenses = managerInputValue + coachInputValue;
+   if (!managerInputValue && !coachInputValue) {
+      alert('provide the input fill');
+      return;
+   } else if (isNaN(managerAndCoachExpenses)) {
+      alert('Please type invalid number');
+      return;
+   }
+
+   const elementTotalValueId = document.getElementById('total');
+   const elementTotalValueString = elementTotalValueId.innerText;
+   const previousTotalExpenses = parseFloat(elementTotalValueString);
+
+   const elementValueId = document.getElementById('player-expenses-total');
+   const elementValueString = elementValueId.innerText;
+   const previousPlayerExpenses = parseFloat(elementValueString);
+
+   const totalExpenses = previousPlayerExpenses + managerAndCoachExpenses + previousTotalExpenses;
+   elementTotalValueId.innerText = totalExpenses;
+
 
 })
